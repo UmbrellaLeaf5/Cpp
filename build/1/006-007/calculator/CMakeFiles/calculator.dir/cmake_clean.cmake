1bdb787f31a8294a13ca017efcf232e633937bf1
Umbrella_Leaf <krivoruchko.di@phystech.edu>
2023-09-26 15:45:51 +0300

    CMake update.

---
+++ build/1/006-007/calculator/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/calculator08buggy.obj"
+  "CMakeFiles/calculator.dir/calculator08buggy.obj.d"
+  "calculator.exe"
+  "calculator.exe.manifest"
+  "calculator.pdb"
+  "libcalculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
