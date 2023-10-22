55f60496c131be481ae49106763ab6069303829f
Umbrella_Leaf <krivoruchko.di@phystech.edu>
2023-10-22 20:37:52 +0300

    The final version of "calculator" (fixing visual input, ^Z problem, "sqrt", and other).

--- 1/CALCULATOR/main_grammar_functions.cpp
+++ 1/CALCULATOR/main_grammar_functions.cpp
@@ -8,31 +8,29 @@
 
 double primary() 
 {
-	Token received_token = ts.get();
-	switch (received_token.kind) 
+	Token rec_t = ts.get();
+	switch (rec_t.kind) 
 	{
 		// кейс для выражение в скобках
 		case '(':
 		{	
-			double received_expression = expression(); // вызываем чтение выражения
-			received_token = ts.get();
-			if (received_token.kind != ')')
-			{
-				ts.unget(received_token);
+			double rec_expr = expression(); // вызываем чтение выражения
+			rec_t = ts.get();
+			if (rec_t.kind != ')'){
+				ts.unget(rec_t);
 				error("')' expected");
 			}
-			return received_expression;
+			return rec_expr;
 		}
 		case '{':
 		{	
-			double received_expression = expression(); // вызываем чтение выражения
-			received_token = ts.get();
-			if (received_token.kind != '}') 
-			{
-				ts.unget(received_token);
+			double rec_expr = expression(); // вызываем чтение выражения
+			rec_t = ts.get();
+			if (rec_t.kind != '}'){
+				ts.unget(rec_t);
 				error("'}' expected");
 			}
-			return received_expression;
+			return rec_expr;
 		}
 		// кейс для отрицательного первичного выражения
 		case '-': 
@@ -42,74 +40,77 @@ double primary()
 			return primary();
 		// кейс для числа
 		case number:
-			return received_token.value;
+			return rec_t.value;
 		// кейс для уже существующей переменной
 		case name: 
 		{
-			if(!nm.is_declared(received_token.name)) // проверяем существование переменной в списке имён
-				error(received_token.name + ": is not declared");
-			Token received_token_2 = ts.get(); // вспомогательный токен для получения значения после левой части
+			Token rec_t_2 = ts.get(); // вспомогательный токен для получения значения после левой части
 			// проверяем, что мы вводим после имени перменной
-			if(received_token_2.kind == '='){ 
+			if(rec_t_2.kind == '='){ 
 				double var_value = expression();
-				return nm.set_value(received_token.name, var_value);
+				return nm.set_value(rec_t.name, var_value);
 			}
 			else{
-				ts.unget(received_token_2); // возвращаем вспомогательный токен в поток
-				return nm.get_value(received_token.name);
+				ts.unget(rec_t_2); // возвращаем вспомогательный токен в поток
+				return nm.get_value(rec_t.name);
 			}
 		}
 		// кейс для квадратного корня
 		case square_root:
 		{
-			received_token = ts.get();
-			if (received_token.kind == '(') 
+			rec_t = ts.get();
+			if (rec_t.kind == '(') 
 			{
-				double received_expression = expression();
-				received_token = ts.get();
-				if (received_token.kind != ')')
+				double rec_expr = expression();
+				rec_t = ts.get();
+				if (rec_t.kind != ')'){
+					ts.unget(rec_t);
 					error("'(' expected");
-				if (received_expression >= 0) // проверка на то, что берем корень неотрицательного числа
-					return sqrt(received_expression);
-				else
+				}
+				if (rec_expr >= 0) // проверка на то, что берем корень неотрицательного числа
+					return sqrt(rec_expr);
+				else{
+					ts.unget(rec_t);
 					error("square root of a negative number");
+				}
 			}
-		else
-			error("'(' expected");
+			else{
+				ts.unget(rec_t);
+				error("'(' expected");
+			}
 		}
 		// кейс для возведения в степень
 		case power:
 		{
-			received_token = ts.get();
-			if (received_token.kind == '(') 
+			rec_t = ts.get();
+			if (rec_t.kind == '(') 
 			{
 				double base_number = expression(); // переменная - основание
-				received_token = ts.get();
-				if (received_token.kind != comma) // за первым аргументом должна следовать запятая
-				{
-					ts.unget(received_token.kind);
+				rec_t = ts.get();
+				if (rec_t.kind != comma){ // за первым аргументом должна следовать запятая
+					ts.unget(rec_t);
 					error("',' expected");
 				}
 				// переменная - показатель
 				int power_number = narrow_cast<int>(expression()); // "сужение" значения с потерей информации и генерацией исключения в противном случае
-				received_token = ts.get();
-				if (received_token.kind != ')')
-				{
-					ts.unget(received_token.kind);
+				rec_t = ts.get();
+				if (rec_t.kind != ')'){
+					ts.unget(rec_t);
 					error("')' expected");
 				}
 				return pow(base_number, power_number);
 			}
-			else
-			{
-				ts.unget(received_token.kind);
+			else{
+				ts.unget(rec_t);
 				error("'(' expected");
 			}
 		}
 		// кейс, если мы не получили, чего хотели
 		default:
-			ts.unget(received_token);
+		{
+			ts.unget(rec_t);
 			error("primary expected");
+		}
 	}
 }
 
@@ -118,8 +119,8 @@ double term()
 {
 	double left_part = primary();
 	for(;;) {
-		Token received_token = ts.get();
-		switch (received_token.kind) 
+		Token rec_t = ts.get();
+		switch (rec_t.kind) 
 		{
 			// кейс для умножения
 			case '*':
@@ -130,25 +131,29 @@ double term()
 			// кейс для деления
 			case '/':
 			{
-				double received_right_part = primary(); // считывание первичного выражения справа
-				if (received_right_part == 0) // ошибка, связанная с делением на ноль
+				double right_part = primary(); // считывание первичного выражения справа
+				if (right_part == 0){ // ошибка, связанная с делением на ноль
+					ts.unget(rec_t);
 					error("/: divide by zero");
-				left_part /= received_right_part;
+				}
+				left_part /= right_part;
 				break;
 			}
 			// кейс для остатка от деления
 			case '%':
 			{
-				double received_right_part = primary(); // считывание первичного выражения справа
-				if (received_right_part == 0) // ошибка, связанная с делением на ноль
+				double right_part = primary(); // считывание первичного выражения справа
+				if (right_part == 0){ // ошибка, связанная с делением на ноль
+					ts.unget(rec_t);
 					error("%: divided by zero");
-				left_part = fmod(left_part, received_right_part); // фукция из cmath, вычисляющая остаток с плавающей запятой
+				}
+				left_part = fmod(left_part, right_part); // фукция из cmath, вычисляющая остаток с плавающей запятой
 				break;
 			}
 			// кейс, когда терм закончился
 			default:
 			{
-				ts.unget(received_token); // возвращаем токен в поток
+				ts.unget(rec_t); // возвращаем токен в поток
 				return left_part;
 			}
 		}
@@ -158,33 +163,35 @@ double term()
 double expression() 
 {
 	double left_part = 0;
-	Token received_token = ts.get();
-	if (received_token.kind == name) {
-		Token received_token_2 = ts.get(); // дополнительный токен для корректного получения значения переменной
+	Token rec_t = ts.get();
+	if (rec_t.kind == name) 
+	{
+		if(!nm.is_declared(rec_t.name)) // проверяем существование переменной в списке имён
+			error(rec_t.name + ": is not declared");
+		Token rec_t_2 = ts.get(); // дополнительный токен для корректного получения значения переменной
 		// если получаем на вход '=' это присваивание
-		if (received_token_2.kind == '=') {
+		if (rec_t_2.kind == '=') {
 			double var_value = expression();
-			left_part = nm.set_value(received_token.name, var_value);
+			left_part = nm.set_value(rec_t.name, var_value);
 		}
-		// если получаем на вход операции терма, то... не вышло исправить баг, связанный с этим :(
-		else if (received_token_2.kind == '*' || received_token_2.kind == '/' || received_token_2.kind == '%')
+		else if (rec_t_2.kind == '*' || rec_t_2.kind == '%' || rec_t_2.kind == '/')
 		{
-			ts.unget(received_token_2);
-			ts.unget(received_token);
-			left_part = term();
+			ts.unget(rec_t_2);
+			ts.unget(rec_t);
+			left_part = primary();
 		}
 		else {
-			ts.unget(received_token_2);
-			left_part = nm.get_value(received_token.name);
+			ts.unget(rec_t_2);
+			left_part = nm.get_value(rec_t.name);
 		}
 	}
 	else {
-		ts.unget(received_token);
+		ts.unget(rec_t);
 		left_part = term();
 	}
 	for(;;) {
-		received_token = ts.get();
-		switch (received_token.kind) {
+		rec_t = ts.get();
+		switch (rec_t.kind) {
 			// кейс для сложения
 			case '+':
 				left_part += term(); // не объявляю правый терм за отдельную переменную
@@ -195,7 +202,7 @@ double expression()
 				break;
 			// кейс, когда выражение закончилось
 			default: 
-				ts.unget(received_token); // возвращаем токен в поток
+				ts.unget(rec_t); // возвращаем токен в поток
 				return left_part;
 		}
 	}
@@ -203,26 +210,26 @@ double expression()
 
 double declaration(bool is_const)  
 {
-	Token received_token = ts.get();
-	if (received_token.kind != name){ // полученное при вводе не соотв. заданным правилам для имени переменной
-		ts.unget(received_token); // возвращаем токен в поток
+	Token rec_t = ts.get();
+	if (rec_t.kind != name){ // полученное при вводе не соотв. заданным правилам для имени переменной
+		ts.unget(rec_t); // возвращаем токен в поток
 		error("name expected in declaration");
 	}
-	string var_name = received_token.name; // присваиваем строке имя полученного из потока токена
-	Token received_token_2 = ts.get(); // используем второй токен, чтобы проверить присваивание значения для переменной
-	if (received_token_2.kind != '='){
-		ts.unget(received_token_2);
+	string var_name = rec_t.name; // присваиваем строке имя полученного из потока токена
+	Token rec_t_2 = ts.get(); // используем второй токен, чтобы проверить присваивание значения для переменной
+	if (rec_t_2.kind != '='){
+		ts.unget(rec_t_2);
 		error("=: missing in declaration of ", var_name);
 	}
-	double received_var_value = expression(); // получаем значение переменной - выражение
-	nm.define_name(var_name, received_var_value, is_const); // при успехе - запоминаем переменную в программу
-	return received_var_value;
+	double var_value = expression(); // получаем значение переменной - выражение
+	nm.define_name(var_name, var_value, is_const); // при успехе - запоминаем переменную в программу
+	return var_value;
 }
 
 double statement() 
 {
-	Token received_token = ts.get();
-	switch (received_token.kind) 
+	Token rec_t = ts.get();
+	switch (rec_t.kind) 
 	{
 		// кейс для объявления
 		case let: 
@@ -232,7 +239,7 @@ double statement()
 			return declaration(true);
 		// кейс для выражения
 		default:
-			ts.unget(received_token);
+			ts.unget(rec_t);
 			return expression();
 	}
 }
