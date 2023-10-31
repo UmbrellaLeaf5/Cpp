4c288894db4008f008f87069a022984a31d445c8
Umbrella_Leaf <krivoruchko.di@phystech.edu>
2023-10-31 17:44:38 +0300

    Adding some ideas.

--- 1/016/logic_scheme_inheritance/logic.h
+++ 1/016/logic_scheme_inheritance/logic.h
@@ -5,14 +5,15 @@ class Operation;
 class Element
 {
     public:
-        Element(State output_state, const std::vector <Output>& elem_outputs);
+        //Element(State output_state, const std::vector <Output>& elem_outputs);
         void set_output_state (State st);
         State output_state() const;
         const std::vector <Output>& outputs const;
         Element& operator >> (Operation& rhs);
 
     protected:
-        virtual void calc()=0;
+        Element(State output_state, const std::vector <Output>& elem_outputs);
+        //virtual void calc()=0;
         bool out {false};
 
     private:
@@ -25,7 +26,39 @@ class Source : public Element
     public:
         Source(Signal sig, State output_state = State::direct, 
                     const std::vector <Output>& outputs = {});
-
-        void set_signal (Signal sig);
-        Source& operator= (Signal sig);
+        //void calc() override {}
 };
+
+class Operation : public Element
+{
+    public:
+        using Element::Element;
+        Connection operator~ (); //const?
+        const std::vector <Input>& inputs() const;
+    
+    protected: 
+        virtual void calc() = 0;
+    
+    private:
+        std::vector <Input> elem_inputs;
+};
+
+class And : public Operation
+{
+    public:
+        using Operation::Operation;
+    
+    protected:
+        void calc() override;
+};
+
+/* 
+вспоминаем, как она когда-то работала:
+src1 >> and1 >> or 2;
+        and1 >> ~or2;
+*/
+
+/*
+void set_signal (Signal sig);
+Source& operator= (Signal sig);
+*/
