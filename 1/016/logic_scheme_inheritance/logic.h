a726a6b87e79e0e93276047f983ffd2f6cea09d9
Umbrella_Leaf <krivoruchko.di@phystech.edu>
2023-11-06 19:06:07 +0300

    Add definition of missing classes

--- 1/016/logic_scheme_inheritance/logic.h
+++ 1/016/logic_scheme_inheritance/logic.h
@@ -1,30 +1,46 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Operation;
+
+enum class State
+{
+    direct = 0,
+    inverted
+};
+
+using Output = std::vector<std::reference_wrapper<Element>>;
+
+class Input;
 
 class Element
 {
   public:
-    // Element(State output_state, const std::vector <Output>& elem_outputs);
+    // Element(State output_state, const vector <Output>& elem_outputs);
     void set_output_state (State st);
     State output_state () const;
-    const std::vector<Output>& outputs const;
+    const vector<Output>& outputs () const;
     Element& operator>> (Operation& rhs);
+    bool out{false};
 
   protected:
-    Element(State output_state, const std::vector<Output>& elem_outputs);
+    Element(State output_state, const vector<Output>& elem_outputs);
     // virtual void calc()=0;
-    bool out{false};
+    // bool out{false};
 
   private:
     State out_state;  // инвертированно или нет
-    std::vector<Output> elem_outputs;
+    vector<Output> elem_outputs;
 };
 
 class Source : public Element
 {
   public:
-    Source(Signal sig, State output_state = State::direct, const std::vector<Output>& outputs = {});
+    Source(bool sig, State output_state = State::direct, const vector<Output>& outputs = {});
     // void calc() override {}
 };
 
@@ -32,14 +48,16 @@ class Operation : public Element
 {
   public:
     using Element::Element;
-    Connection operator~();  // const?
-    const std::vector<Input>& inputs () const;
+
+    Input operator~() { return {*this, State::inverted}; }
+
+    const vector<Input>& inputs () const;
 
   protected:
     virtual void calc () = 0;
 
   private:
-    std::vector<Input> elem_inputs;
+    vector<Input> elem_inputs;
 };
 
 class And : public Operation
@@ -49,6 +67,32 @@ class And : public Operation
 
   protected:
     void calc () override;
+};
+
+class Or : public Operation
+{
+  public:
+    using Operation::Operation;
+
+  protected:
+    void calc () override;
+};
+
+class Input
+{
+  public:
+    Input(Element& elem, State st);
+
+    Element& element () { return m_elem; }
+
+    const Element& element () const { return m_elem; }
+
+    bool signal () const;
+    State state () const;
+
+  private:
+    reference_wrapper<Element> m_elem;
+    const bool is_inv;
 };
 
 /*
