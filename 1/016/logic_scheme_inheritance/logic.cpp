91054266c224abad7eb5b01f87630c5ca5abf808
Umbrella_Leaf <krivoruchko.di@phystech.edu>
2023-11-06 23:11:28 +0300

    Start to write a class realization for logic

--- 1/016/logic_scheme_inheritance/logic.cpp
+++ 1/016/logic_scheme_inheritance/logic.cpp
@@ -1,1 +1,11 @@
 #include "logic.h"
+
+Input::Input(Element& elem, State st) : m_elem{elem}, is_inv{st == State::inverted} {}
+
+bool Input::signal() const
+{
+    bool res = element().out;
+    return is_inv ? !res : res;
+}
+
+State Input::state() const { return is_inv ? State::inverted : State::direct; }
