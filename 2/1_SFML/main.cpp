5373f15a5b61aadbc4f3a9cf0c2c66f35725acb6
Umbrella_Leaf <krivoruchko.di@phystech.edu>
2023-12-31 01:58:22 +0300

    try SFML for fun

---
+++ 2/1_SFML/main.cpp
@@ -0,0 +1,27 @@
+// g++ -c main.cpp -ID:/Programs/SFML/SFML-2.6.1/include -DSFML_STATIC
+// g++ main.o -o main -LD:/Programs/SFML/SFML-2.6.1/lib -lsfml-graphics-s -lsfml-window-s
+// -lsfml-system-s -lopengl32 -lfreetype -lwinmm -lgdi32 -mwindows -lsfml-main
+#include <SFML/Graphics.hpp>
+
+int main ()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
