d509fff5e0ebae1f5ca1e5c613cbf59fa3041f08
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-09-21 12:52:13 +0300

    update semesters folders

--- semesters/2/smth/1_SFML/main.cpp
+++ semesters/2/smth/1_SFML/main.cpp
@@ -1,6 +1,7 @@
 // g++ -c main.cpp -ID:/Programs/SFML/SFML-2.6.1/include -DSFML_STATIC
-// g++ main.o -o main -LD:/Programs/SFML/SFML-2.6.1/lib -lsfml-graphics-s -lsfml-window-s
-// -lsfml-system-s -lopengl32 -lfreetype -lwinmm -lgdi32 -mwindows -lsfml-main
+// g++ main.o -o main -LD:/Programs/SFML/SFML-2.6.1/lib -lsfml-graphics-s
+// -lsfml-window-s -lsfml-system-s -lopengl32 -lfreetype -lwinmm -lgdi32
+// -mwindows -lsfml-main
 #include <SFML/Graphics.hpp>
 
 int main() {
@@ -11,8 +12,7 @@ int main() {
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
-        window.close();
+      if (event.type == sf::Event::Closed) window.close();
     }
 
     window.clear();
