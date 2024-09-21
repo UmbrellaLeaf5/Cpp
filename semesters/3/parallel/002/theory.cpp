81b275f18da3731115402080f085bdd47c169eb1
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-09-21 12:05:57 +0300

    finish lesson_2

--- semesters/3/parallel/002/theory.cpp
+++ semesters/3/parallel/002/theory.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
   st = MPI_Init(&argc, &argv);
   if (st != MPI_SUCCESS) {
     // ...
-    MPI_Abort(MPI_COMM_WORLD);
+    MPI_Abort(MPI_COMM_WORLD, st);
     // ...
   }
   MPI_Finalize();
